f97a15bc9b678d9480dc510c9748e904d2754b6f
Mrinal Mallik <mrinal@nagorik.tech>
2024-03-18 20:59:49 +0600

    dotenv

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,8 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["nuxt-windicss"],
+  runtimeConfig: {
+    apiKey: process.env.API_KEY,
+    apiBaseUrl: process.env.API_BASE_URL,
+  },
 });
